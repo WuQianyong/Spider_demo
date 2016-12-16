1be353ac7b1c59926f5d85857a9aa7b02e5e44af
VsatYong <wuqianyong@163.com>
2016-12-16 18:01:26 +0800

    add algorithm

---
+++ algorithm_demo/README.md
@@ -0,0 +1,5 @@
+### 数据结构与算法实例
+
+这里面是一个关于Python数据结构和算法的 demo
+
+requirement: Python3.5.2
---
+++ algorithm_demo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env Python3
+# -*- coding: utf-8 -*-
+# @Time    : 2016/12/16 17:58
+# @Author  : wuqianyong
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
+
+
